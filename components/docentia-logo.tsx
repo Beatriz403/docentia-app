5a01284c8a59b44e5727913e4bc191c0b409f083
Beatriz403 <84119017+Beatriz403@users.noreply.github.com>
2025-12-02 17:06:36 +0100

    Update docentia-logo.tsx

--- components/docentia-logo.tsx
+++ components/docentia-logo.tsx
@@ -1,7 +1,7 @@
 export function DocentIALogo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center gap-3 ${className}`}>
-      <img src="/images/logo_transparente.png" alt="DocentIA Logo" className="h-4 w-auto" />
+      <img src="/images/logo_transparente.png" alt="DocentIA Logo" className="h-12 w-auto" />
     </div>
   )
 }
