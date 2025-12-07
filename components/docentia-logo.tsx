d8d8b219eaa10c161292cb5363456e085b528c67
Beatriz del Pozo <84119017+Beatriz403@users.noreply.github.com>
2025-12-07 02:38:35 +0100

    Add files via upload

--- components/docentia-logo.tsx
+++ components/docentia-logo.tsx
@@ -1,7 +1,7 @@
 export function DocentIALogo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center gap-3 justify-start  ${className}`}>
-      <img src="/images/logo_transparente.png" alt="DocentIA Logo" className="h-12 w-auto" />
+      <img src="/images/logo_transparente.png" alt="DocentIA Logo" className="h-16 md:h-20 w-auto" />
     </div>
   )
 }
